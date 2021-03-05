98a5f2b890a9ce175b1aea56737937d90ac9c8ce
silkylaroux <damian.mikayla@gmail.com>
2021-03-05 02:19:51 -0700

    added first problem

---
+++ 2021/mar/week1/template.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"fmt"
+)
+
+func fastGetInt(b []byte) int {
+	neg := false
+	if b[0] == '-' {
+		neg = true
+		b = b[1:]
+	}
+	var n int
+	n = 0
+	for _, v := range b {
+		n = n*10 + int(v-'0')
+	}
+	if neg {
+		return - n
+	}
+	return n
+}
+
+//========== Implement Algorithm =======================
+func solution() string {
+	return ""
+}
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	//================== Variables used ====================
+
+	//======================================================
+
+	//============== Get return value from File ============
+	if len(os.Args) >= 2 {
+
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+		scanner = bufio.NewScanner(file)
+		defer file.Close()
+
+	}
+	scanner.Split(bufio.ScanWords)
+	//======================= I/O ==========================
+
+	//======================================================
+	
+	//==================== OUTPUT ==========================
+
+}
