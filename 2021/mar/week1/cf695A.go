95329ebff176429b1aef65b72b94d5e5513213a8
silkylaroux <damian.mikayla@gmail.com>
2021-03-05 23:53:22 -0700

    started problem 3

---
+++ 2021/mar/week1/cf695A.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"fmt"
+	"math"
+)
+
+func fastGetInt(b []byte) int {
+	neg := false
+	if b[0] == '-' {
+		neg = true
+		b = b[1:]
+	}
+	var n int
+	n = 0
+	for _, v := range b {
+		n = n*10 + int(v-'0')
+	}
+	if neg {
+		return - n
+	}
+	return n
+}
+
+//========== Implement Algorithm =======================
+func solution(val int) int {
+
+	return int(math.Pow(float64(10),float64(val)))-val
+}
+
+func main() {
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	//================== Variables used ====================
+	var n, val int
+	//======================================================
+
+	//============== Get return value from File ============
+	if len(os.Args) >= 2 {
+
+		file, err := os.Open(os.Args[1])
+		if err != nil {
+			log.Fatalf("failed opening file: %s", err)
+		}
+		scanner = bufio.NewScanner(file)
+		defer file.Close()
+
+	}
+	scanner.Split(bufio.ScanWords)
+	//======================= I/O ==========================
+	scanner.Scan()
+	n = fastGetInt(scanner.Bytes())
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		val = fastGetInt(scanner.Bytes())
+		fmt.Println(solution(val))
+	}
+	//======================================================
+	
+	//==================== OUTPUT ==========================
+
+}
